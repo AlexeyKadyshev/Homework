b78202aac131d954e95c6430a70966f153c0a952
Alexey <A.Kadyshev@yandex.ru>
2023-03-24 11:04:30 +0300

    Add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 != 0 || num == 0)
+{
+    Console.Write("нет");
+}
+else
+{
+    Console.Write("да");
+}
