991d25b101d6ee20d651bf52c138568ddaae2e9f
Alexey <A.Kadyshev@yandex.ru>
2023-03-24 09:59:38 +0300

    Add gitignore and Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первоe число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.Write("max = " + num1);
+}
+else
+{
+    Console.Write("max = " + num2);
+}
