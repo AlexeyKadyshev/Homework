678ce9bf4c1069a04b9331b2fe87786d77208784
Alexey <A.Kadyshev@yandex.ru>
2023-03-24 11:36:20 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число:");
+int num = int.Parse(Console.ReadLine());
+int even_num = 2;
+if (num != 0)
+{
+    while (even_num <= num)
+    {
+        Console.WriteLine(even_num);
+        even_num = even_num + 2;
+    }
+}
+else
+{
+    Console.Write("0 не является натуральным числом");
+}
